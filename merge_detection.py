bf98250f0790de7887a48aff6ca6e5f3f707cf29
Eightina <frankalation@gmail.com>
2022-09-13 16:24:41 +0800

    upd

--- merge_detection.py
+++ merge_detection.py
@@ -23,54 +23,51 @@ from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, smart_inference_mode
 from BinOpenpose import pyopenpose as op
 
-
-
 def run(        
-        weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
-        source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
-        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-        imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=True,  # show results
-        save_txt=True,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_crop=False,  # save cropped prediction boxes
-        nosave=False,  # do not save images/videos
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        augment=False,  # augmented inference
-        visualize=False,  # visualize features
-        update=False,  # update all models
-        project=ROOT / 'output/detect',  # save results to project/name
-        name='exp',  # save results to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
-        half=False,  # use FP16 half-precision inference
-        dnn=False,  # use OpenCV DNN for ONNX inference
-        vid_stride=1,  # video frame-rate stride):
-        model_folder = "OpenposeModels"
+        weights = ROOT / 'weights/yolov5s.pt',  # model.pt path(s)
+        source = ROOT / 'input/videos/railway-construction-site.avi',  # file/dir/URL/glob, 0 for webcam
+        data = ROOT / 'data/coco128.yaml',  # dataset.yaml path
+        imgsz = (640, 640),  # inference size (height, width)
+        conf_thres = 0.25,  # confidence threshold
+        iou_thres = 0.45,  # NMS IOU threshold
+        max_det = 1000,  # maximum detections per image
+        device = '',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img = True,  # show results
+        save_txt = True,  # save results to *.txt
+        save_conf = False,  # save confidences in --save-txt labels
+        save_crop = False,  # save cropped prediction boxes
+        nosave = False,  # do not save images/videos
+        classes = None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms = False,  # class-agnostic NMS
+        augment = False,  # augmented inference
+        visualize = False,  # visualize features
+        update = False,  # update all models
+        project = ROOT / 'output/detect',  # save results to project/name
+        name = 'exp',  # save results to project/name
+        exist_ok = False,  # existing project/name ok, do not increment
+        line_thickness = 3,  # bounding box thickness (pixels)
+        hide_labels = False,  # hide labels
+        hide_conf = False,  # hide confidences
+        half = True,  # use FP16 half-precision inference
+        dnn = True,  # use OpenCV DNN for ONNX inference
+        vid_stride = 1,  # video frame-rate stride):
+        model_folder = "OpenposeModels"  # bin folder name of openpose models
 ):
     # Starting OpenPose
-    params = dict()
-    params["model_folder"] = "OpenposeModels"
+    op_params = dict()
+    op_params["model_folder"] = model_folder
     opWrapper = op.WrapperPython()
-    opWrapper.configure(params)
+    opWrapper.configure(op_params)
     opWrapper.start()
-    # yolo
+    # Starting yolo
     device = select_device(device)
     model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
     stride, names, pt = model.stride, model.names, model.pt
     imgsz = check_img_size(imgsz, s=stride)  # check image size
     
-    
     datum = op.Datum()
-    # cap = cv2.VideoCapture("../../../examples/media/jzh/720P.flv")
-    cap = cv2.VideoCapture("./input/videos/railway-construction-site.avi")
+    cap = cv2.VideoCapture(source)
+    # cap = cv2.VideoCapture("./input/videos/railway-construction-site.avi")
     fps = cap.get(cv2.CAP_PROP_FPS)
     size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     framecount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
@@ -85,8 +82,9 @@ def run(
             
             
             
+            
             opframe = datum.cvOutputData
-            cv2.imshow("Site Danger Detection based on OpenPose 1.7.0", opframe)
+            cv2.imshow("Site Danger Detection based on OpenPose 1.7.0 & YOLOv5s", opframe)
             videoWriter.write(opframe)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -97,8 +95,8 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
-    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'weights/yolov5s.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default=ROOT / 'input/videos/railway-construction-site.avi', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
@@ -121,10 +119,10 @@ def parse_opt():
     parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
     parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--half', default=True, action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', default=True, action='store_true', help='use OpenCV DNN for ONNX inference')
     parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
-    parser.add_argument('--model-folder', type=str, default="OpenposeModels", help='bin folder name of op models')
+    parser.add_argument('--model-folder', type=str, default="OpenposeModels", help='bin folder name of openpose models')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     print_args(vars(opt))
@@ -151,8 +149,6 @@ def main(opt):
     #         print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
     #         raise e
     try:
-
-
         check_requirements(exclude=('tensorboard', 'thop'))
         run(**vars(opt))
         
